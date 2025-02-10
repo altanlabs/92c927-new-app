caa6180401281bd36e2a1d24162ef1caf6475179
Altan <albert@altan.ai>
2025-02-10 11:10:49 +0100

    Initial commit

---
+++ src/routes.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Index from "./pages/index";
+import NotFound from "./pages/NotFound";
+import RootBoundary from "./components/errors/RootBoundary";
+
+const routes = [
+  {
+    path: "/",
+    element: (<></>),
+    errorElement: <RootBoundary />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
