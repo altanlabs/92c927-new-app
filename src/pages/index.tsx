e22c05a144f4a00ddf1de449e46137f96671130a
altanlabs <interfaces@altan.ai>
2025-02-10 13:27:30 +0000

    New commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,9 @@
-import { MysticInterface } from "../components/blocks/MysticInterface";
-import { ChatOverlay } from "../components/blocks/ChatOverlay";
+import { CircularMenu } from "../components/blocks/CircularMenu";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-background to-background/50 relative overflow-hidden">
-      {/* Background effects */}
-      <div className="absolute inset-0">
-        <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,rgba(var(--primary),0.1),transparent_70%)]" />
-      </div>
-
-      {/* Main content */}
-      <div className="relative z-10">
-        <MysticInterface />
-        <ChatOverlay />
-      </div>
-    </div>
+    <main className="min-h-screen bg-black">
+      <CircularMenu />
+    </main>
   );
 }
