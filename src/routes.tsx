7230c9abcdadcb9b2bc27bd52bfc34ffb4346b8d
altanlabs <interfaces@altan.ai>
2025-02-10 12:40:34 +0000

    New commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
 import RootBoundary from "./components/errors/RootBoundary";
+import Layout from "./layout";
 
 const routes = [
   {
     path: "/",
-    element: (<></>),
+    element: <Layout />,
     errorElement: <RootBoundary />,
     children: [
       {
